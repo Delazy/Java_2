6643aaa39b3aa6ed1a9ec71573a8b7d0fdfac7eb
Liza <delazyf@gmail.com>
2020-03-03 00:00:41 +0300

    Initial commit

---
+++ src/by/belstu/it/Kubik/TextFunction.java
@@ -0,0 +1,18 @@
+package by.belstu.it.Kubik;
+
+public class TextFunction
+{
+    public String getValue() {
+            return "HI!";
+        }
+
+    public int getFild() {
+        return fild;
+    }
+
+    public void setFild(int fild) {
+        this.fild = fild;
+    }
+
+    int fild;
+}
